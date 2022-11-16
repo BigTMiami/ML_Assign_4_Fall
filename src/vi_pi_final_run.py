7ac213a72480c195d4d611db8b6c95154423e859
Anthony Menninger <tony.menninger@gmail.com>
2022-11-16 09:54:20 -0500

    Final run work

--- src/vi_pi_final_run.py
+++ src/vi_pi_final_run.py
@@ -56,11 +56,65 @@ compare_vi_pi(S_max=500, gamma_range=[0.5, 0.9, 0.99, 0.999, 0.9999, 0.99999])
 
 
 ###############################
-# LAKE
+# LAKE - MEDIUM
 ###############################
 gamma = 0.9
 e_stop = 0.0001
 map_name = "Medium"
+map_used = maps[map_name]
+is_slippery = True
+P, R = example.openai("FrozenLake-v1", desc=map_used, is_slippery=is_slippery)
+title_settings = (
+    f"{map_name}, Gamma:{gamma}, {'Is' if is_slippery else 'Not'} Slippery, E Stop:{e_stop}"
+)
+
+vi = mdp.ValueIteration(P, R, gamma, epsilon=e_stop)
+vi_info = vi.run()
+
+pi = mdp.PolicyIteration(P, R, gamma, max_iter=100)
+pi_info = pi.run()
+
+lake_plot_policy_and_value(vi.policy, vi.V, title_settings, suptitle="Lake VI", show_policy=True)
+
+lake_plot_policy_and_value(pi.policy, pi.V, title_settings, suptitle="Lake PI", show_policy=True)
+
+compare_two_policies_and_values(
+    pi_info[-1]["Policy"],
+    vi_info[-1]["Policy"],
+    pi_info[-1]["Value"],
+    vi_info[-1]["Value"],
+    title_settings,
+    "Lake PI and VI Differences",
+)
+
+chart_reward_vs_error(vi_info, title_settings, "Lake VI Reward and Error", location=lake_location)
+chart_reward_vs_error(pi_info, title_settings, "Lake PI Reward and Error", location=lake_location)
+
+plot_gamma_iterations(
+    [0.1, 0.5, 0.9, 0.99],
+    "Lake VI Gamma Comparison",
+    map_name,
+    is_slippery,
+    "vi",
+    show_policy=True,
+)
+
+compare_policy_iterations(vi_info, "Lake VI Policy Comparison", map_used)
+compare_policy_iterations(pi_info, "Lake PI Policy Comparison", map_used, max_iteration=12)
+
+plot_policy_value_iterations(
+    pi_info, "Lake PI", map_used, seperate_charts=False, iters_to_use=[8, 9, 10, 11]
+)
+plot_policy_value_iterations(
+    vi_info, "Lake VI", map_used, seperate_charts=False, iters_to_use=[0, 3, 6, 9, 13]
+)
+
+###############################
+# LAKE - Large
+###############################
+gamma = 0.9
+e_stop = 0.0001
+map_name = "Large"
 map_used = maps[map_name]
 is_slippery = True
 P, R = example.openai("FrozenLake-v1", desc=map_used, is_slippery=is_slippery)
